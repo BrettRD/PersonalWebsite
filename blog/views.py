438c7d63d21ebb3292e62441b4366e63603a592e
BrettRD <brett.downing1@gmail.com>
2016-04-03 22:47:05 +0800

    added auth check on new/edited posts

--- blog/views.py
+++ blog/views.py
@@ -17,11 +17,12 @@ def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
+            if request.user.is_authenticated():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.published_date = timezone.now()
+                post.save()
+                return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
@@ -31,11 +32,12 @@ def post_edit(request, pk):
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
+            if request.user.is_authenticated():
+                post = form.save(commit=False)
+                post.author = request.user
+                #post.published_date = timezone.now()
+                post.save()
+                return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
