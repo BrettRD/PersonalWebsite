774ff8674d328c03155551019c13cc546bb11e45
BrettRD <brett.downing1@gmail.com>
2016-04-04 14:17:51 +0800

    set login pages, 404 on unpublished

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from .models import Post
 from .forms import PostForm
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from django.shortcuts import redirect
 
 # Create your views here.
@@ -10,34 +12,69 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+    #post = get_object_or_404(Post, pk=pk)
+    try:
+        post = Post.objects.get(pk=pk)
+    except Post.DoesNotExist:
+        raise Http404("Post does not exist")
+    if not request.user.is_authenticated():
+        if not post.published_date:
+            raise Http404("Post does not exist") #do not allude to unpublished posts.
     return render(request, 'blog/post_detail.html', {'post': post})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
-            if request.user.is_authenticated():
-                post = form.save(commit=False)
-                post.author = request.user
-                post.published_date = timezone.now()
-                post.save()
-                return redirect('post_detail', pk=post.pk)
+            #if request.user.is_authenticated():
+            post = form.save(commit=False)
+            post.author = request.user
+            #post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
-            if request.user.is_authenticated():
-                post = form.save(commit=False)
-                post.author = request.user
-                #post.published_date = timezone.now()
-                post.save()
-                return redirect('post_detail', pk=post.pk)
+            #if request.user.is_authenticated():
+            post = form.save(commit=False)
+            post.author = request.user
+            #post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('blog.views.post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog.views.post_list')
+
+@login_required
+def post_unpublish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.unpublish()
+    return redirect('blog.views.post_detail', pk=pk)
+    #return redirect('blog.views.post_draft_list')
+
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', auth_views.login),
+    #url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', auth_views.logout, {'next_page': '/'}),
+    #url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'', include('blog.urls')),
 ]
